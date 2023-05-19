e689c3155c9bf6eb91e9a2c06a3f49a2edfe6aeb
unlsycn <g.humphrey.sln@gmail.com>
2023-05-19 17:23:26 +0800

    feat(minisdl): implement IMG_Load

--- libs/libSDL_image/src/image.c
+++ libs/libSDL_image/src/image.c
@@ -12,7 +12,21 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+  FILE *fp = fopen(filename, "r");
+  if (!fp)
+      return NULL;
+
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+
+  void *buf = malloc(size);
+  fread(buf, size, 1, fp);
+  SDL_Surface *sf = STBIMG_LoadFromMemory(buf, size);
+  free(buf);
+  fclose(fp);
+
+  return sf;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
